2c932c4492f7e068cfc5e3d070bb09f3ee6639d5
yoon664 <yoonj0264@gmail.com>
2025-04-06 12:14:28 +0900

    .

--- src/step/step05.tsx
+++ src/step/step05.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 const Step05 = () => {
-  return (
-    <div>Step05</div>
-  )
+    const divRef = useRef<HTMLDivElement>(null);
+    /*
+        HTMLInputElement
+        HTMLButtonElement
+
+    */
+    
+    const handleEvent = (e : React.MouseEvent<HTMLButtonElement>) => {
+        console.log(e)
+    }
+    /*
+        KeyboardEvent
+        React.ChangeEvent
+        React.FocusEvent
+    */
+
+    return (
+        <>
+            <div ref={divRef}>Step05</div>
+            <button onClick={handleEvent}>CLICK</button>
+        </>
+    )
 }
 
 export default Step05
